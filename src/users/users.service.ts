52c1568127480a7cb82e26f53b8279d99dc5ef80
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-10-12 16:29:21 +0300

    Add users logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+
+export type User = any;
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1, name: "Misha", password: "qwerty123"
+        }, {
+            userId: 2, name: "PvlMrzc", password: "qwerty123"
+        },
+    ]
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.name === username)
+    }
+}
