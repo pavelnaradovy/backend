9737b2746ba11e2f7d170f8f00497e9c6cb35c73
Pavel Naradovy <pavel.narodovy@beanmachine.dev>
2023-10-17 14:03:42 +0300

    Add jwt strategy to providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
-  providers: [AuthService, LocalStrategy,],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [UsersModule, PassportModule, JwtModule.register({
     secret: jwtConstants.secret,
     signOptions: { expiresIn: '60s' },
